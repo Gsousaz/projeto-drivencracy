0126528f4662ddd6841d3dbbf51e23c79748fabe
Gsousaz <doomlighthighlord@gmail.com>
2023-07-18 01:01:45 -0300

    feat: rota get /polls criada

--- src/controllers/polls.controller.js
+++ src/controllers/polls.controller.js
@@ -5,11 +5,23 @@ export async function newPoll(req, res) {
     const { title, expireAt } = req.body;
     const poll = { title, expireAt };
     const insertResult = await db.collection("polls").insertOne(poll);
-    
-    const insertedPoll = await db.collection("polls").findOne({ _id: insertResult.insertedId });
+
+    const insertedPoll = await db
+      .collection("polls")
+      .findOne({ _id: insertResult.insertedId });
 
     res.status(201).send(insertedPoll);
     console.log(insertedPoll);
+  } catch (err) {
+    (err) => res.status(500).send(err);
+  }
+}
+
+export async function showPolls(req, res) {
+  try {
+    const polls = await db.collection("polls").find().toArray();
+    console.log(polls);
+    res.status(200).send(polls);
   } catch (err) {
     res.status(500).send(err);
   }
--- src/routes/polls.router.js
+++ src/routes/polls.router.js
@@ -1,10 +1,11 @@
 import { Router } from "express";
 import { validateSchema } from "../middlewares/validateSchema.middleware.js";
-import { newPoll } from "../controllers/polls.controller.js";
+import { newPoll, showPolls } from "../controllers/polls.controller.js";
 import { schemaPoll } from "../schemas/pollSchema.js";
 
 const pollsRouter = Router();
 
 pollsRouter.post("/poll", validateSchema(schemaPoll), newPoll);
+pollsRouter.get("/poll", showPolls)
 
 export default pollsRouter
