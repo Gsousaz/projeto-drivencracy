2a67378acb0e47eeae5541eaa0cbaca3fa6a49a5
Gsousaz <doomlighthighlord@gmail.com>
2023-07-18 23:56:45 -0300

    feat: criada rota GET /poll/:id/choice

--- src/controllers/choices.controller.js
+++ src/controllers/choices.controller.js
@@ -9,7 +9,9 @@ export async function newVote(req, res) {
     const pollExists = await db.collection("polls").findOne({ _id: objectId });
     if (!pollExists) return res.sendStatus(404);
 
-    const titleExists = await db.collection("choices").findOne({ title: title });
+    const titleExists = await db
+      .collection("choices")
+      .findOne({ title: title });
     if (titleExists) return res.sendStatus(409);
 
     const insertResult = await db
@@ -20,8 +22,39 @@ export async function newVote(req, res) {
       .findOne({ _id: insertResult.insertedId });
 
     res.status(201).send(insertedPoll);
-    console.log(insertedPoll)
+    console.log(insertedPoll);
   } catch (err) {
     res.status(500).send(err.message);
+  }
+}
+
+export async function showPollOptions(req, res) {
+  try {
+    const id = req.params.id;
+
+    // Validação do ID
+    if (!ObjectId.isValid(id)) {
+      return res.status(400).send("ID inválido");
+    }
+
+    const objectId = new ObjectId(id);
+    console.log("ID:", id);
+    console.log("ObjectId:", objectId);
+
+    const pollExists = await db.collection("polls").findOne({ _id: objectId });
+    if (!pollExists) {
+      return res.sendStatus(404);
+    }
+
+    const pollOptions = await db
+      .collection("choices")
+      .find({ pollId: id })
+      .toArray();
+
+    console.log("pollOptions:", pollOptions);
+    return res.sendStatus(200);
+  } catch (err) {
+    console.error("Erro:", err);
+    return res.status(500).send(err.message);
   }
 }
--- src/routes/choices.router.js
+++ src/routes/choices.router.js
@@ -1,10 +1,11 @@
 import { Router } from "express";
-import { newVote } from "../controllers/choices.controller.js";
+import { newVote, showPollOptions } from "../controllers/choices.controller.js";
 import { schemaChoice } from "../schemas/choiceSchema.js";
 import { validateSchema } from "../middlewares/validateSchema.middleware.js";
 
 const choicesRouter = Router();
 
 choicesRouter.post("/choice", validateSchema(schemaChoice), newVote);
+choicesRouter.get("/poll/:id/choice", showPollOptions)
 
 export default choicesRouter;
